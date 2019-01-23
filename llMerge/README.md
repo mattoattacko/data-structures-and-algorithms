d23b251eec0f82ea97d27a1d2ddfd4dc46049fed
Matto <matt.mcquain@gmail.com>
2019-01-22 21:40:05 -0800

    trying to fix tests

---
+++ llMerge/README.md
@@ -0,0 +1,11 @@
+# Merge two Linked Lists
+* Code Challenge 08: Merge two linked lists
+
+## Challenge
+* Write a function called `mergeLists` which takes two linked lists as arguments. Zip the two linked lists together into one so that the nodes alternate between the two lists and return a reference to the head of the zipped list. Try and keep additional space down to O(1). You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## Solution
+![whiteboarding](whiteboard)
---
+++ llMerge/linked-lists.js
@@ -0,0 +1,129 @@
+'use strict';
+
+const Node = require('./node.js');
+
+class LinkedList {
+
+  constructor () {
+    this.head = null;
+  }
+  append(value) {
+    let node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      return this; 
+    }
+    let current = this.head;
+    while(current.next) {
+      current = current.next;
+    }
+    current.next = node;
+    return this;
+  }
+  prepend(value) {
+    let node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      return this; 
+    }
+    node.next = this.head;
+    this.head = node; 
+  }
+
+  // insertBefore(3, 2)
+  
+  insertBefore(value, newValue) {
+    let node = new Node(newValue);
+    let current = this.head;
+    let previous;
+
+    while(current.next) {
+      if (current.value === value){
+        // if it's the head
+        if (current === this.head) {
+          node.next = current;
+          this.head = node;
+          return this;
+        }
+        // if it's a middle value
+        previous.next = node;
+        node.next = current;
+        return this;
+      }
+      previous = current;
+      current = current.next;
+    }
+    // if it's the tail
+    if (current.next === null) {
+      previous.next = node;
+      node.next = current;
+    }
+    return this;
+  }
+
+  insertAfter(value, newVal){
+    let node = new Node(newVal);
+    let current = this.head;
+
+    while(current.next) {
+      if(current.value === value) {
+        node.next = current.next; // the next value of the new value equals the next value of the current value - making space for newVal
+        current.next = node;
+        return this;
+      }
+      current = current.next;
+    }
+    // if it's the tail
+    if(current.next === null) {
+      current.next = node;
+      return this;
+    }
+  }
+
+  kthFromEnd(k) {
+    
+    console.log('value of k:', k);
+    let length = 0;
+    let current = this.head;
+    let arr = [];
+    
+    while(current){
+      arr.push(current.value);
+      current = current.next;
+      length++;
+    }
+    
+    console.log('length:', length); 
+    console.log('arr:', arr);  
+    let newIndex = (length - 1) - k;
+    console.log('new index:', newIndex);
+    console.log('correct answer:', arr[newIndex]);
+    return arr[newIndex];
+  }
+
+  reverse() {
+    let current = this.head;
+    let next = null;
+    let previous = null;
+  
+    while(current) {
+      next = current.next;
+      current.next = previous;
+      previous = current;
+      current = next;
+    }
+    this.head = previous;
+  }
+
+  length() {
+    let length = 0;
+    let current = this.head;
+    while(current){
+      current = current.next;
+      length++;
+    }
+    return length;
+  }
+}
+
+module.exports = LinkedList;
